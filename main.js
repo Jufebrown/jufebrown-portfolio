33624b2416733873ce519c6d01b725b73de66303
Jufebrown <jufebrown@gmail.com>
2017-08-26 10:48:12 -0500

    worked with objects on contact info

--- main.js
+++ main.js
@@ -3,6 +3,68 @@ var contactArray = [
   '<li><a href="https://www.linkedin.com/in/jufe-brown-tsai-3a2701126/">linkedIn</a></li>'
 ]
 
+var contactObj = {
+    "name": {
+        "firstName": "Professor",
+        "lastName": "Chaos"
+    },
+    "email": "professor@professorchaos.com",
+    "twitter": "@ProfChaos",
+    "quote": "Prepare to meet thy doom"
+}
+
+contactObj['phone'] = '555-555-5555'
+contactObj['facebook'] = 'myfacebook'
+
+var quote = 'Carpe Diem'
+contactObj['quote'] = quote
+
+contactObj.quote = [
+    "Prepare to meet thy doom",
+    "Nobody expects Professor Chaos",
+    "Bow before the power of the DARK SIDE"
+]
+
+// contactObj.getGithub = function() {
+//   return contactObj.github
+// }
+
+console.log(contactObj)
+
+var elementLister = function(contactElement) {
+  debugger
+  var listString = "<li>" + contactElement + "</li>"
+
+  if (Array.isArray(contactElement)) {
+    listString = "<ol>"
+
+    contactElement.forEach(function(element) {
+      listString += "<li>" + element + "</li>";
+    })
+
+    listString += '</ol>'
+
+  } else if(typeof contactElement === "object"){
+    listString = "<li>";
+
+    for(var element in contactElement){
+      debugger
+      listString += contactElement[element] + " ";
+    }
+
+    listString += "</li>";
+  }
+
+  $("#contact-list").append(listString);
+};
+
+// for(var contactType in contactObj){
+//   elementLister( contactObj[contactType] );
+// }
+
+
+
+
 // var i = 0
 //
 //
